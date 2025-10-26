7e1fe147450d9fe6c87afe9a98c9e71e67a58af6
Sándor Kakuk <53403093+sandorkakuk@users.noreply.github.com>
2025-10-26 02:49:20 +0100

    Implement comparison of two numbers
    Added comparison between two numbers and their output.

--- beginner/if.js
+++ beginner/if.js
@@ -1,6 +1,11 @@
-// JavaScript
-if (kor >= 18) {
-    console.log("Felnőtt vagy.");
+//Két szám közűl melyik a naygobb?
+let a = 42;
+let b = 17;
+
+if (a > b) {
+    console.log("Az A a nagyobb szám:", a);
+} else if (b > a) {
+    console.log("A B a nagyobb szám:", b);
 } else {
-    console.log("Még nem vagy felnőtt.");
+    console.log("A két szám egyenlő:", a);
 }
