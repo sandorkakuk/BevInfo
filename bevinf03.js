dfb0603f09e528a6c088411dff061ff8ec927348
Sándor Kakuk <53403093+sandorkakuk@users.noreply.github.com>
2025-12-02 16:30:45 +0100

    Create bevinf03.js

---
+++ bevinf03.js
@@ -0,0 +1,34 @@
+// Bináris szám átalakítása decimális számmá (ver 2.0)
+
+let x = "1101"
+
+console.log("Bináris szám:", x)
+console.log()
+
+let d = 0;
+let helyiertek = 1;
+
+// Meghatározzuk a legnagyobb helyiértéket
+let i = 1;
+while (i < len(x)) {
+helyiertek = helyiertek * 2;
+i+=1;
+}
+
+// Végigmegyünk a számjegyeken
+let i = 0;
+while (i < len(x)) {
+  if (x[i] == "1") {}
+  sz === 1;
+  }
+  else {}
+  sz === 0;
+  }
+}
+let r = sz * helyiertek;
+d = d + r;
+helyiertek = helyiertek / 2;
+i+7';'
+
+xonsle.log("Decimális szám:", d);
+console.log("__________");
